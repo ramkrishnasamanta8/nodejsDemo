70fbc12fa1a1b05a998bc6848afa4ca4cc6547ad
ramkrishnasamanta8 <40237180+ramkrishnasamanta8@users.noreply.github.com>
2020-02-17 14:50:52 +0530

    Update production.js

--- config/environments/production.js
+++ config/environments/production.js
@@ -1,7 +1,7 @@
 module.exports = {
     host: "127.0.0.1",
-    port: 1234, // change with production port
-    mongoUrl: process.env.CONNECTION_STRING,
+    port: process.env.PORT, // change with production port
+    mongoUrl: "mongodb+srv://ram:ram123@cluster0-eywvt.mongodb.net/test?retryWrites=true&w=majority",
     logLevel: process.env.LOG_LEVEL,
     secret: process.env.SECRET,
 };
